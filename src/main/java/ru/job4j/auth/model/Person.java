b50523cc1aad09b1501cb37007aea980efd5754d
ftptpf <Nb49Rtdd>
2023-03-03 22:16:03 +0300

    PersonController

--- src/main/java/ru/job4j/auth/model/Person.java
+++ src/main/java/ru/job4j/auth/model/Person.java
@@ -1,12 +1,17 @@
 package ru.job4j.auth.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "persons")
 public class Person {
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
